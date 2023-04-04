f0ccdd757c74614468f013abc874b2b458781959
Iñigo Moreno <inigo.moreno@smallworldfs.com>
2023-04-04 15:59:32 +0200

    Refactor DemoApplication by adding Student class and StudentController

--- spring_boot_demo/src/main/java/com/example/demo/DemoApplication.java
+++ spring_boot_demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,17 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,5 +19,4 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
